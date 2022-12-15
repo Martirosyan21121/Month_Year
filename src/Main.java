9054a0ec51ad75e72fd9e190d0ed5e64204929a0
Martirosyan21121 <narekmartirosyanbm21@gmile.com>
2022-12-15 22:22:46 +0400

    new1

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Input a month number: ");
+        System.out.println("Input a month number: "); // until 12
         int month = scanner.nextInt();
 
         System.out.println("Input a year: ");
@@ -14,57 +14,58 @@ public class Main {
         String monthName = "T";
 
         switch (month) {
-            case 1:
+            case 1 -> {
                 monthName = "January";
                 daysInMonth = 31;
-                break;
-            case 2:
+            }
+            case 2 -> {
                 monthName = "February";
                 if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
                     daysInMonth = 29;
                 } else {
                     daysInMonth = 30;
                 }
-                break;
-            case 3:
+            }
+            case 3 -> {
                 monthName = "March";
                 daysInMonth = 31;
-                break;
-            case 4:
+            }
+            case 4 -> {
                 monthName = "April";
                 daysInMonth = 30;
-                break;
-            case 5:
+            }
+            case 5 -> {
                 monthName = "May";
                 daysInMonth = 31;
-                break;
-            case 6:
+            }
+            case 6 -> {
                 monthName = "June";
                 daysInMonth = 30;
-                break;
-            case 7:
+            }
+            case 7 -> {
                 monthName = "July";
                 daysInMonth = 31;
-                break;
-            case 8:
+            }
+            case 8 -> {
                 monthName = "August";
                 daysInMonth = 31;
-                break;
-            case 9:
+            }
+            case 9 -> {
                 monthName = "September";
                 daysInMonth = 30;
-                break;
-            case 10:
+            }
+            case 10 -> {
                 monthName = "October";
                 daysInMonth = 31;
-                break;
-            case 11:
+            }
+            case 11 -> {
                 monthName = "November";
                 daysInMonth = 30;
-                break;
-            case 12:
+            }
+            case 12 -> {
                 monthName = "December";
                 daysInMonth = 31;
+            }
         }
         System.out.print(monthName + " " + year + " has " + daysInMonth + " days\n");
 
